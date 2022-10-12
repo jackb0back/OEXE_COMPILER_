ebac35496d3d14cca7792905140e1fd3f3cdfe99
jackb0back <54455407+jackb0back@users.noreply.github.com>
2022-10-12 17:24:20 -0400

    Update Main.js

--- OEXE_HUB/Repos/0/Main.js
+++ OEXE_HUB/Repos/0/Main.js
@@ -1,1 +1,2 @@
-Load("https://cdn.jsdelivr.net/gh/jackb0back/OEXE_COMPILER_@main/OEXE_HUB/OEXES/0/TerminalTest.js");
+Load("TerminalTest","https://cdn.jsdelivr.net/gh/jackb0back/OEXE_COMPILER_@main/OEXE_HUB/OEXES/0/TerminalTest.js");
+Load("EaglerCraft","https://cdn.jsdelivr.net/gh/jackb0back/OEXE_COMPILER_@main/OEXE_HUB/OEXES/0/Eagler.js");

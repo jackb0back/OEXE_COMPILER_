d08824455498777a80acebdb6409770a2a078a28
jackb0back <54455407+jackb0back@users.noreply.github.com>
2022-10-14 17:23:36 -0400

    Create Main.js

---
+++ OEXE_HUB/Repos/1/Main.js
@@ -0,0 +1,3 @@
+var Items;
+Items.push({ Name: "DriveTest", src: "1ev0SnAgaIcVsWrkmCDVnS4xwpSgO2hmH", oexe: "1ev0SnAgaIcVsWrkmCDVnS4xwpSgO2hmH", type: "Drive"});
+Load("TerminalTest","https://cdn.jsdelivr.net/gh/jackb0back/OEXE_COMPILER_@main/OEXE_HUB/OEXES/0/TerminalTest.js");

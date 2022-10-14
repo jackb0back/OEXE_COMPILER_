8b9f625d3e822662fdb83dc5f2e291f65f3cbb9a
jackb0back <54455407+jackb0back@users.noreply.github.com>
2022-10-14 19:50:31 -0400

    Create Main.js

---
+++ OEXE_HUB/Repos/2/Main.js
@@ -0,0 +1,11 @@
+var Items;
+//for a google drive item
+//Items.push({Name: "", src: "", oexe: "", type: "Drive"}) SRC AND OEXE IS THE DRIVE FILE ID
+//Items.push({ Name: "DriveTest", src: "1ev0SnAgaIcVsWrkmCDVnS4xwpSgO2hmH", oexe: "1ev0SnAgaIcVsWrkmCDVnS4xwpSgO2hmH", type: "Drive"});
+Items.push({Name: "EaglerCraft", src: "1W0CzzosmkOStEz3jpKAillzMWLozYeig", oexe: "1W0CzzosmkOStEz3jpKAillzMWLozYeig", type: "Drive"});
+Items.push({Name: "Five nights at winstons", src: "1hDTwCDfVckBDSud7yxn3X1VLHfFDQozP", oexe: "1hDTwCDfVckBDSud7yxn3X1VLHfFDQozP", type: "Drive"});
+Items.push({Name: "Apple", src: "1zZqeUkbYv2Y39yUl9TqywASppDOSqa7Q", oexe: "1zZqeUkbYv2Y39yUl9TqywASppDOSqa7Q", type: "Drive"});
+Items.push({Name: "Getting over it", src: "1t6iTZ0nMlp0Ujbz9fpHfMNDLKs6gzcNy", oexe: "1t6iTZ0nMlp0Ujbz9fpHfMNDLKs6gzcNy", type: "Drive"});
+Items.push({Name: "Scratch to html", src: "1WSNQriGAez0xyrYjA00s5m43pOyYB6Ow", oexe: "1WSNQriGAez0xyrYjA00s5m43pOyYB6Ow", type: "Drive"});
+Items.push({Name: "Scratch terraria", src: "1Yz5IDO67r4GUfdnbeGqeodFTgs8XEwxr", oexe: "1Yz5IDO67r4GUfdnbeGqeodFTgs8XEwxr", type: "Drive"});
+Items.push({Name: "Everyone's sky", src: "1L-FgPmemmHHjWMyHENUNAr5ExYlXDBEC", oexe: "1L-FgPmemmHHjWMyHENUNAr5ExYlXDBEC", type: "Drive"});

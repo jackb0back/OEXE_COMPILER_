bcdd7c27db68a1121ef7d9a874fb08486218a107
jackb0back <54455407+jackb0back@users.noreply.github.com>
2022-10-14 17:50:28 -0400

    Update Main.js

--- OEXE_HUB/Repos/1/Main.js
+++ OEXE_HUB/Repos/1/Main.js
@@ -1,3 +1,5 @@
 var Items;
+//for a google drive item
 Items.push({ Name: "DriveTest", src: "1ev0SnAgaIcVsWrkmCDVnS4xwpSgO2hmH", oexe: "1ev0SnAgaIcVsWrkmCDVnS4xwpSgO2hmH", type: "Drive"});
+//for an external item
 Load("TerminalTest","https://cdn.jsdelivr.net/gh/jackb0back/OEXE_COMPILER_@main/OEXE_HUB/OEXES/0/TerminalTest.js");

1352fa6854c4d187f36314fe8b2444a9204c6be4
jackb0back <54455407+jackb0back@users.noreply.github.com>
2022-10-16 11:48:47 -0400

    Create Main.js

---
+++ OEXE_HUB/StudioRepos/0/Main.js
@@ -0,0 +1,5 @@
+var Items;
+//for a google drive item
+//Items.push({Name: "", src: "", oexe: "", type: "Drive"}) SRC AND OEXE IS THE DRIVE FILE ID
+//Items.push({ Name: "DriveTest", src: "1ev0SnAgaIcVsWrkmCDVnS4xwpSgO2hmH", oexe: "1ev0SnAgaIcVsWrkmCDVnS4xwpSgO2hmH", type: "Drive"});
+Items.push({ Name: "StudioTest", src: "12xyqp692vIkuJmCdnVR_YXVNlXoU1Xzk", oexe: "12xyqp692vIkuJmCdnVR_YXVNlXoU1Xzk", type: "Drive"});
